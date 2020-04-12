a2749d96d8c4b1f860fd10385ad3dbc9530284cb
mbryles <mbryles@gmail.com>
2020-04-12 15:40:06 -0400

    more mods to the heroku deployment script

--- heroku.sh
+++ heroku.sh
@@ -1,7 +1,5 @@
+# cribbed liberally from Mait Raible's heroku deployment script found here: https://github.com/oktadeveloper/spring-boot-react-example/blob/master/heroku.sh
 #!/bin/bash
-# Warning: this script has only been tested on macOS Sierra. There's a good chance
-# it won't work on other operating systems. If you get it working on another OS,
-# please send a pull request with any changes required. Thanks!
 set -e
 
 cd `dirname $0`
@@ -26,74 +24,12 @@ else
 fi
 serverUri="https://$server_app.herokuapp.com"
 
-#if ! echo "$(git remote -v)" | grep -q devcon-react-client; then
-#  client_app=devcon-react-client
-#  heroku create -r client $client_app
-#else
-#  client_app=$(heroku apps:info -r client --json | python -c 'import json,sys;print json.load(sys.stdin)["app"]["name"]')
-#fi
-#clientUri="https://$client_app.herokuapp.com"
-
-# replace the client URL in the server
-#sed -i -e "s|http://localhost:3000|$clientUri|g" $r/server/src/main/java/com/okta/developer/demo/beer/BeerController.java
-
-# Deploy the server
-#cd $r/server
-#mvn clean package -DskipTests
-./gradlew clean build
+./gradlew clean build -x test
 
 mv build/libs/*jar build/libs/${server_app}.jar
 
 heroku deploy:jar build/libs/*jar -r server -o "--server.port=\$PORT"
 heroku config:set -r server FORCE_HTTPS="true"
 
-# Deploy the client
-#cd $r/client
-#rm -rf build
-# replace the server URL in the client
-#sed -i -e "s|http://localhost:8080|$serverUri|g" $r/client/src/BeerList.tsx
-#yarn && yarn build
-#cd build
-#
-#cat << EOF > static.json
-#{
-#  "https_only": true,
-#  "root": ".",
-#  "routes": {
-#    "/**": "index.html"
-#  }
-#}
-#EOF
-#
-#rm -f ../dist.tgz
-#tar -zcvf ../dist.tgz .
-
-# TODO replace this with the heroku-cli-static command `heroku static:deploy`
-#source=$(curl -n -X POST https://api.heroku.com/apps/$client_app/sources -H 'Accept: application/vnd.heroku+json; version=3')
-#get_url=$(echo "$source" | python -c 'import json,sys;print json.load(sys.stdin)["source_blob"]["get_url"]')
-#put_url=$(echo "$source" | python -c 'import json,sys;print json.load(sys.stdin)["source_blob"]["put_url"]')
-#curl "$put_url" -X PUT -H 'Content-Type:' --data-binary @../dist.tgz
-#cat << EOF > build.json
-#{
-#  "buildpacks": [{ "url": "https://github.com/heroku/heroku-buildpack-static" }],
-#  "source_blob": { "url" : "$get_url" }
-#}
-#EOF
-#build_out=$(curl -n -s -X POST https://api.heroku.com/apps/$client_app/builds \
-#  -d "$(cat build.json)" \
-#  -H 'Accept: application/vnd.heroku+json; version=3' \
-#  -H "Content-Type: application/json")
-#output_stream_url=$(echo "$build_out" | python -c 'import json,sys;print json.load(sys.stdin)["output_stream_url"]')
-#curl -s -L "$output_stream_url"
-#
-#rm build.json
-#rm ../dist.tgz
-
-# cleanup changed files
-#sed -i -e "s|$serverUri|http://localhost:8080|g" $r/client/src/BeerList.tsx
-#sed -i -e "s|$clientUri|http://localhost:3000|g" $r/server/src/main/java/com/okta/developer/demo/beer/BeerController.java
-#rm $r/client/src/BeerList.tsx-e
-#rm $r/server/src/main/java/com/okta/developer/demo/beer/BeerController.java-e
-
 # show apps and URLs
-#heroku open -r client
+heroku open -r server
